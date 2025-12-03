705f1210020d1988b68973ea5bc89e92aa87dd6a
xuhuanzy <501417909@qq.com>
2025-12-03 16:58:34 +0800

    feat: 添加自定义断言函数注册功能
    添加 use 函数用于注册自定义断言函数，允许用户扩展断言功能

--- luaassert/init.lua
+++ luaassert/init.lua
@@ -3,12 +3,33 @@
 local expect = require('luaassert.expect')
 require("luaassert.matchers.matchers")
 require("luaassert.matchers.spyMatchers")
+local Assertion = require("luaassert.assertion")
 local mock = require("luaassert.spy.mock")
+local util = require("luaassert.util")
+
+---@type table<function, true>
+local used = {}
+
+-- 注册自定义断言函数
+---@param fn fun(exports: table, util: table)
+---@return table @ 导出表
+local function use(fn)
+    local exports = {
+        Assertion = Assertion,
+        util = util,
+    }
+    if not used[fn] then
+        fn(exports, util)
+        used[fn] = true
+    end
+
+    return exports
+end
 
 local Api = {
     expect = expect,
     mock = mock,
+    use = use,
 }
-
 
 return Api
